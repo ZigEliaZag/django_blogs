b264acac4125a94147383d5f4edfba592af4686a
ZigEliaZag <eliakaliza50@icloud.com>
2025-11-10 18:26:43 +0100

    profile update

--- codestarr/codestars/settings.py
+++ codestarr/codestars/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = 'django-insecure-ratx$%hrchcl6d++)b7*%ougft1#wxu@1_8!+@rt^7g@b%7#z2'
+#SECRET_KEY = 
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 
