271b73ba104a19bc16da5d360c3ba8a6e293db94
ZigEliaZag <eliakaliza50@icloud.com>
2025-11-11 16:00:29 +0100

    changed access

--- codestar/settings.py
+++ codestar/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-from decouple import config
+#from decouple import config
 from pathlib import Path
 import os 
 import dj_database_url
@@ -24,8 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ.get('SECRET_KEY')
-SECRET_KEY = config('SECRET_KEY') 
+SECRET_KEY = os.environ.get('SECRET_KEY')
+#SECRET_KEY = config('SECRET_KEY') 
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
