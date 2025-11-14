07b68ef2d105b684bcf65b2c9971083b5a4e3323
ZigEliaZag <eliakaliza50@icloud.com>
2025-11-15 00:59:34 +0100

    new files

---
+++ blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import generic
+from .models import Post
+
+# Create your views here.
+class PostList(generic.ListView):
+    queryset = Post.objects.all()
+    queryset = Post.objects.filter(status=1)
+    template_name = "post_list.html"
